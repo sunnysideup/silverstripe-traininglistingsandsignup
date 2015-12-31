ad54ceb81150f457134610c77fc9d969576c9652
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:37:35 +1300

    Converted to PSR-2

--- code/TrainingHolder.php
+++ code/TrainingHolder.php
@@ -1,65 +1,72 @@
 <?php
 
-class TrainingHolder extends Page {
+class TrainingHolder extends Page
+{
 
-	private static $icon = "mysite/images/treeicons/TrainingHolder";
+    private static $icon = "mysite/images/treeicons/TrainingHolder";
 
-	//parents and children in sitetree
-	private static $allowed_children = array("TrainingPage"); //can also be "none";
-	private static $default_child = "TrainingPage";
+    //parents and children in sitetree
+    private static $allowed_children = array("TrainingPage"); //can also be "none";
+    private static $default_child = "TrainingPage";
 
-	public function canCreate($member = null) {
-		return TrainingHolder::get()->count() ? false: true;
-	}
+    public function canCreate($member = null)
+    {
+        return TrainingHolder::get()->count() ? false: true;
+    }
 
-	public function canDelete($member = null) {
-		return false;
-	}
+    public function canDelete($member = null)
+    {
+        return false;
+    }
 
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-		return $fields;
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        return $fields;
+    }
 }
 
-class TrainingHolder_Controller extends Page_Controller {
+class TrainingHolder_Controller extends Page_Controller
+{
 
-	/**
-	 *
-	 * @return ArrayList
-	 */ 
-	function MonthlyCourses() {
-		$results = new ArrayList();
+    /**
+     *
+     * @return ArrayList
+     */
+    public function MonthlyCourses()
+    {
+        $results = new ArrayList();
 
-		$stage = Versioned::current_stage();
-		$suffix = (!$stage || $stage == 'Stage') ? "" : "_$stage";
+        $stage = Versioned::current_stage();
+        $suffix = (!$stage || $stage == 'Stage') ? "" : "_$stage";
 
-		$sqlResults = DB::query("
+        $sqlResults = DB::query("
 			SELECT DISTINCT MONTH(\"Date\") AS \"Month\", YEAR(\"Date\") AS \"Year\"
 			FROM \"SiteTree$suffix\" NATURAL JOIN \"TrainingPage$suffix\"
 			WHERE \"ParentID\" = ".$this->ID." AND \"Date\" > CONVERT_TZ(now(),'+00:00','+13:00')
 			ORDER BY \"Year\" DESC, \"Month\" ASC;"
-		);
+        );
 
-		if($sqlResults) foreach($sqlResults as $sqlResult) {
-			$month = (isset($sqlResult['Month'])) ? (int) $sqlResult['Month'] : 1;
-			$year = ($sqlResult['Year']) ? (int) $sqlResult['Year'] : date('Y');
+        if ($sqlResults) {
+            foreach ($sqlResults as $sqlResult) {
+                $month = (isset($sqlResult['Month'])) ? (int) $sqlResult['Month'] : 1;
+                $year = ($sqlResult['Year']) ? (int) $sqlResult['Year'] : date('Y');
 
-			$date = DBField::create_field('Date', array(
-				'Day' => 1,
-				'Month' => $month,
-				'Year' => $year
-			));
+                $date = DBField::create_field('Date', array(
+                'Day' => 1,
+                'Month' => $month,
+                'Year' => $year
+            ));
 
 
-			$results->push(new ArrayData(array(
-				'Date' => $date,
-				'Courses' => TrainingPage::get()
-					->filter(array("ShowInMenus" => 1))
-					->where(" MONTH(\"TrainingPage\".\"Date\") = $month AND YEAR(\"TrainingPage\".\"Date\") = $year")
-			)));
-		}
-		return $results;
-	}
-
+                $results->push(new ArrayData(array(
+                'Date' => $date,
+                'Courses' => TrainingPage::get()
+                    ->filter(array("ShowInMenus" => 1))
+                    ->where(" MONTH(\"TrainingPage\".\"Date\") = $month AND YEAR(\"TrainingPage\".\"Date\") = $year")
+            )));
+            }
+        }
+        return $results;
+    }
 }
